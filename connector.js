b59c4b595e3e392ff3e3954e5b4f75133ceb937f
jem <jhonemillan@gmail.com>
2017-12-19 00:46:21 -0500

    queries

--- connector.js
+++ connector.js
@@ -1,18 +1,27 @@
 const MongoClient = require('mongodb').MongoClient;
+var ObjectID = require('mongodb').ObjectID;
 
 MongoClient.connect('mongodb://localhost:27017/test',(err, db)=>{
     if(err){ return console.log('unable to connect to data base');}
 
     console.log('connected.');
 
-    db.db('test').collection('users').insertOne({
-        nombre: 'jhon edwin millan',
-        email: 'jhonemillan@gmail.com'
-    },(err, res)=>{
-        if(err) {return console.log('unable to save data');}
+    // db.db('test').collection('users').insertOne({
+    //     nombre: 'jhon millan',
+    //     email: 'jhon@gmail.com'
+    // },(err, res)=>{
+    //     if(err) {return console.log('unable to save data');}
 
-        console.log('user saved.');
+    //     console.log(JSON.stringify(res.ops));
+    // });
+
+    db.db('test').collection('users').find({
+        _id: new ObjectID("5a389a1b11bc7d3d89759aa6")
+    }).toArray().then((rows)=>{
+        console.log(rows);
     });
+
+    
 
     db.close();
 })
